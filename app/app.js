9b3d5faf374e397ca3c6a27d1ab95ac986fcae82
James Fickel <jamesfickel@gmail.com>
2015-01-10 02:12:59 -0600

    added pubnub code - demo works locally :)

--- app/app.js
+++ app/app.js
@@ -5,14 +5,20 @@ import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+var userID = Math.round(Math.random() * 1000);
+
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  PubNub: window.PubNubEmber.extend({
+    cfg: {
+      subscribe_key: 'sub-c-0e7a8ffa-93af-11e4-85c7-02ee2ddab7fe',
+      publish_key: 'pub-c-5b5be1b3-3307-4dd6-9ee2-f8056a93428c',
+      uuid: userID
+    }
+  })
 });
-
-console.log('hi');
-console.log(window.PubNubEmber);
 
 loadInitializers(App, config.modulePrefix);
 
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,48 @@
 import Ember from 'ember';
-/* global moment */
 
 export default Ember.Controller.extend({
-  init: function() {
-    // console.log("hello from app controller");
-    // console.log(moment);
+  needs: ['pubnub:main'],
+  channel: 'Demo Channel',
+  new_message: '',
+  messages: Ember.ArrayProxy.create({ content: ['Welcome to the Demo Channel'] }),
+  userID: function () {
+    return this.get('pubnub').get('cfg').uuid;
+  }.property(),
+  users: Ember.ArrayProxy.create({ content: [] }),
+  init: function () {
+    var pn   = this.get('pubnub');   // PubNub service instance
+    var chan = this.get('channel');  // channel name
+    var self = this;                 // reference to 'this' for callbacks
+
+    // Subscribe to the Channel
+    pn.emSubscribe({ channel: chan });
+
+    // Register for message events
+    pn.on(pn.emMsgEv(chan), function (payload) {
+      self.get('messages').pushObject(payload.message);
+    });
+
+    // Register for presence events
+    pn.on(pn.emPrsEv(chan), function () {
+      self.get('users').set('content', pn.emListPresence(chan));
+    });
+
+    // Pre-Populate the user list (optional)
+    pn.emHereNow({ channel: chan });
+
+    // Populate message history (optional)
+    pn.emHistory({
+      channel: chan,
+      count: 500
+    });
+  },
+  actions: {
+    publish: function () {
+      this.get('pubnub').emPublish({
+        channel: this.get('channel'),
+        message: "[User" + this.get('userID') + "]: " + this.get('new_message')
+      });
+      this.set('new_message', '');
+    }
   }
 });
